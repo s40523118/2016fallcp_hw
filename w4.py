ef62143d930ac91774775a0af4edc051e2adc9bf
s40523118 <40523118@gm.nfu.edu.tw>
2016-09-30 15:12:48 +0800

    Updated from Brython Server: 2016/9/30 下午3:12:44

--- w4.py
+++ w4.py
@@ -1,3 +1,8 @@
-mystring = ("40523118")
-mystring = mystring + "test"
-print("mystring")
+'''
+mystring = "40523118"
+print(mystring)
+'''
+
+thetext = input("Enter some text ")
+print ("This is what you enterde:")
+print (thetext)
